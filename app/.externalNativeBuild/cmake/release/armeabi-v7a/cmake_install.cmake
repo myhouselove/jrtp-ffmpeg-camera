b2e171385147db9145b437377ca87ab6d398d6d8
wangmingyong <wangmingyong@orbbec.com>
2017-09-12 23:09:11 +0800

    add rtp so

--- app/.externalNativeBuild/cmake/release/armeabi-v7a/cmake_install.cmake
+++ app/.externalNativeBuild/cmake/release/armeabi-v7a/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: G:/opengl/Githubrtp/app
+# Install script for directory: G:/android/jrtp-ffmpeg-camera/app
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "G:/opengl/Githubrtp/app/.externalNativeBuild/cmake/release/armeabi-v7a/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "G:/android/jrtp-ffmpeg-camera/app/.externalNativeBuild/cmake/release/armeabi-v7a/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
